5812189cfbf5df46710a6925d19ad344040fd6b3
Mugesh R <mugesh.r1906@gmail.com>
2024-12-26 22:31:08 +0530

    merging contact

--- scripts.js
+++ scripts.js
@@ -1,43 +1,25 @@
-function applyDarkMode(isDarkMode) {
-
 function darkmode() {
-
   const themeButton = document.getElementById('dark');
   const homeSection = document.querySelector('.home');
   const bubble = document.querySelector('.bubble');
+  const currentImage = themeButton.style.backgroundImage;
 
-  if (isDarkMode) {
-    themeButton.style.backgroundImage = "url('assests/sun.png')";
-    homeSection.style.backgroundColor = '#1e1e1e';
-    homeSection.style.color = '#ffffff';
-    bubble.style.backgroundColor = '#ffffff';
+  if (currentImage.includes('half-moon.png')) {
+      themeButton.style.backgroundImage = "url('assests/sun.png')";
+      homeSection.style.backgroundColor = '#1e1e1e'; 
+      homeSection.style.color = '#ffffff';
+      bubble.style.backgroundColor = '#ffffff';
   } else {
-    themeButton.style.backgroundImage = "url('assests/half-moon.png')";
-    homeSection.style.backgroundColor = '#c0c0c0';
-    homeSection.style.color = '#000000';
-    bubble.style.backgroundColor = '#090909';
+      themeButton.style.backgroundImage = "url('assests/half-moon.png')";
+      homeSection.style.backgroundColor = '#c0c0c0'; 
+      homeSection.style.color = '#000000'; 
+      bubble.style.backgroundColor = '#090909';
   }
 }
-
-
-function darkmode() {
-  const isDarkMode = localStorage.getItem('darkMode') === 'true';
-  const newDarkMode = !isDarkMode; 
-  localStorage.setItem('darkMode', newDarkMode); 
-  applyDarkMode(newDarkMode); 
-}
-
-
-document.addEventListener('DOMContentLoaded', () => {
-  const isDarkMode = localStorage.getItem('darkMode') === 'true';
-  applyDarkMode(isDarkMode);
-});
-
-
 const bubble = document.querySelector('.bubble');
+
 document.addEventListener('mousemove', (event) => {
   const x = event.clientX;
   const y = event.clientY;
   bubble.style.transform = `translate(${x}px, ${y}px)`;
 });
-}
